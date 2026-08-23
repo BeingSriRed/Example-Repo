c0a3ec77e0ce8147cca18961d3330aa37409e900
BeingSriRed <srinath.idam99@gmail.com>
2026-08-23 18:08:21 +0530

    committing example and firstEx

---
+++ firstEx.java
@@ -0,0 +1,11 @@
+public class firstEx {
+    
+    // Your program begins with a call to main()
+    
+    public static void main(String[] args)
+    {
+        // Prints "Hello, World" to the terminal window.
+        
+        System.out.println("My First Ex");
+    }
+}
