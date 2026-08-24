381dda456271d6390880f7598a09f023ad8c9f7c
BeingSriRed <srinath.idam99@gmail.com>
2026-08-24 11:39:22 +0530

    modified from sr_brnach to see the changes

--- firstEx.java
+++ firstEx.java
@@ -4,8 +4,7 @@ public class firstEx {
     
     public static void main(String[] args)
     {
-        // Prints "Hello, World" to the terminal window.
+        // Prints "comment written" to the terminal window.
         
-        System.out.println("My First Ex");
+        System.out.println("My First Ex in the JPL");
     }
-}
